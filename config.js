6f3d2014d9c16b462ab1c36dd42db622f77fb103
Lotfi <Lotfi@DESKTOP-S65O4MC>
2021-03-22 20:42:15 +0100

    update mail

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config  = {
     MONGODB_URI : process.env.MONGODB_URI,
     EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
-    APPLICATION_URL: "https://research-activities.netlify.app"
+    APPLICATION_URL: "https://rs-client-seven.vercel.app/"
 }
 
 module.exports = config;
